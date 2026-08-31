bec61c2f46b80fdcb1acacff48840d3bc504b3a7
Import { Petr } from 'vue <petrsvarc@email.cz>
2026-08-31 20:45:56 +0200

    Add Kodi entry point

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Module: default
+# Author: cache
+# Created on: 10.5.2020
+# License: AGPL v.3 https://www.gnu.org/licenses/agpl-3.0.html
+# Modified for Not Another WebShare Plugin (NAWSP), 2026-08-31.
+
+import sys
+import yawsp
+
+if __name__ == '__main__':
+    yawsp.router(sys.argv[2][1:])
